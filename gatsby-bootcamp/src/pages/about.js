48ba3882024f9cbfa807bdccf2b0f49bb8baaf38
Arif Sanaullah <ak7896023@gmail.com>
2021-01-03 23:25:37 +0500

    first commit

---
+++ gatsby-bootcamp/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+function About() {
+  return (
+    <Layout>
+      <h1>About page is this</h1>
+    </Layout>
+  )
+}
+
+export default About
