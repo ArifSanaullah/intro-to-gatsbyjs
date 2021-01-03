7ed537907e2663726ac1a3eb3520c0108900a3ed
Arif Sanaullah <ak7896023@gmail.com>
2021-01-03 23:23:43 +0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Create by{" "}
+        <a
+          href="https://twitter.com/arifsanaullah78"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Arif Sanaullah
+        </a>{" "}
+        with Love{" "}
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
+import { Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div>
+        <h1>The great gatsby bootcamp</h1>
+        <p>
+          Need a developer? <Link to="/contact">Contact me</Link>{" "}
+        </p>
+        <p>
+          See all blogs <Link to="/blog">Blogs</Link>{" "}
+        </p>
+        <p>
+          Find about me <Link to="/about">About me</Link>{" "}
+        </p>
+      </div>
+    </Layout>
+  )
 }
